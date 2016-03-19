dab11e3acccbc3da64cbd138de773fae88678b86
sumanth2174 <sumanth2174@gmail.com>
2016-03-19 12:41:43 +0530

    1st html

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+<title>details</title>
+</head>
+<body>
+
+<body bgcolor="">
+<font color=""><P>ENTER NUMBER</font color>
+<?php
+$x=5;
+$i=1;
+
+while($i<2)
+{
+$x=$x*$x;
+$i++;
+}
+echo $x;
+?>
+
+</body>
+</html>
